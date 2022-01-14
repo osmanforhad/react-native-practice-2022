16f7839db58020d4dc963177d65d640915dacd48
Osman Forhad <osmanforhad93@gmail.com>
2022-01-14 12:41:20 +0600

    setup react native element component framework and write code for test run

--- App.js
+++ App.js
@@ -1,32 +1,12 @@
-import React from "react"
-import {
-  Container,
-  Text,
-  Heading,
-  Center,
-  NativeBaseProvider,
-} from "native-base"
-export function Example() {
-  return (
-    <Container>
-      <Heading>
-        A component library for the
-        <Heading color="emerald.500"> React Ecosystem</Heading>
-      </Heading>
-      <Text mt="3" fontWeight="medium">
-        NativeBase is a simple, modular and accessible component library that
-        gives you building blocks to build you React applications.
-      </Text>
-    </Container>
-  )
+import React, { Component } from 'react';
+import { Button } from 'react-native-elements';
+
+class App extends Component {
+  render() {
+    return (
+      <Button title="Solid Button" />
+    );
+  }
 }
 
-export default () => {
-  return (
-    <NativeBaseProvider>
-      <Center flex={1} px="3">
-        <Example />
-      </Center>
-    </NativeBaseProvider>
-  )
-}
+export default App;
