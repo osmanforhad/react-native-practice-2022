2bb01f4b4881047cd2b8017628abf87d1851d4bc
Osman Forhad <osmanforhad93@gmail.com>
2022-01-18 16:57:54 +0600

    working with image view concept under flatlist

--- App.js
+++ App.js
@@ -24,23 +24,32 @@ class App extends Component {
   }
 
   ListDataAsJSON = [
-    {title:"Bangladesh", subtitle:"This is the Asian Nation Country"},
-    {title:"India", subtitle:"This is the Asian Nation Country"},
-    {title:"Pakisthan", subtitle:"This is the Asian Nation Country"},
-    {title:"China", subtitle:"This is the Asian Nation Country"},
-    {title:"JAPAN", subtitle:"This is the Asian Nation Country"},
-    {title:"SPAIN", subtitle:"This is the Europian Nation Country"},
-    {title:"Portugal", subtitle:"This is the Europian Nation Country"},
-    {title:"France", subtitle:"This is the Europian Nation Country"},
-    {title:"Poland", subtitle:"This is the Europian Nation Country"},
-    {title:"Indonesia", subtitle:"This is the Asian Nation Country"},
+    {title:"Bangladesh", subtitle:"This is the Asian Nation Country", image: "https://cdn.pixabay.com/photo/2017/11/23/07/47/baby-2972221_960_720.jpg"},
+    {title:"India", subtitle:"This is the Asian Nation Country", image: "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_960_720.jpg"},
+    {title:"Pakisthan", subtitle:"This is the Asian Nation Country", image:"https://cdn.pixabay.com/photo/2021/12/12/01/15/lotus-6863937_960_720.jpg"},
+    {title:"China", subtitle:"This is the Asian Nation Country", image:"https://cdn.pixabay.com/photo/2022/01/13/15/18/man-6935496_960_720.jpg"},
+    {title:"JAPAN", subtitle:"This is the Asian Nation Country", image:"https://cdn.pixabay.com/photo/2021/11/29/17/32/city-6833167_960_720.jpg"},
+    {title:"SPAIN", subtitle:"This is the Europian Nation Country", image:"https://cdn.pixabay.com/photo/2022/01/12/15/40/angel-statue-6933216_960_720.jpg"},
+    {title:"Portugal", subtitle:"This is the Europian Nation Country", image:"https://cdn.pixabay.com/photo/2021/08/02/05/17/sunflowers-6515860_960_720.jpg"},
+    {title:"France", subtitle:"This is the Europian Nation Country", image:"https://cdn.pixabay.com/photo/2021/09/08/02/46/lotus-6605296_960_720.jpg"},
+    {title:"Poland", subtitle:"This is the Europian Nation Country", image:"https://cdn.pixabay.com/photo/2020/07/08/04/12/work-5382501_960_720.jpg"},
+    {title:"Indonesia", subtitle:"This is the Asian Nation Country", image:"https://cdn.pixabay.com/photo/2012/12/09/00/16/abstract-69124_960_720.jpg"},
   ];
 
-  ChildView= ({title, subtitle}) => {
+  ChildView= ({title, subtitle, thambnail}) => {
     return(
-      <View style={{backgroundColor:"white", padding:10, margin:5}}>
-        <Text style={{color:"black", fontSize: 18}}>{title}</Text>
+      <View style={{backgroundColor:"white", margin:5, flex:100, flexDirection:"row"}}>
+
+
+      <View style={{flex:30}}>
+        <Image source={{uri:thambnail}} style={{width:"100%", height:70}} />
+      </View>
+
+      <View style={{flex:70, padding:10}}>
+      <Text style={{color:"black", fontSize: 18}}>{title}</Text>
         <Text style={{color:"black", fontSize: 16}}>{subtitle}</Text>
+      </View>
+
       </View>
     )
   }
@@ -68,7 +77,7 @@ class App extends Component {
         <Button onPress={this.changeInfo} title="Clcik Here" color="teal"></Button>
 
         <View>
-          <FlatList data={this.ListDataAsJSON} renderItem={({item}) => <this.ChildView title={item.title} subtitle={item.subtitle} /> } />
+          <FlatList data={this.ListDataAsJSON} renderItem={({item}) => <this.ChildView title={item.title} subtitle={item.subtitle} thambnail={item.image} /> } />
         </View>
 
         <View>
