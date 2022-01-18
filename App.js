42675fb7b9de4a49f698c32bc45ee690f37fc28f
Osman Forhad <osmanforhad93@gmail.com>
2022-01-18 15:10:55 +0600

    working with react native flat list example

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Button, Image, ScrollView } from 'react-native';
+import { View, Text, Button, Image, ScrollView, FlatList } from 'react-native';
 import CustomStyle from "./asset/style/customStyle";
 import ComponentOne from './components/ComponentOne';
 import PageOne from './pages/PageOne';
@@ -23,6 +23,29 @@ class App extends Component {
     this.setState({name:"Forhad", city:"Sylhet", Profession:"Software Enginearing"})
   }
 
+  ListDataAsJSON = [
+    {title:"Bangladesh", subtitle:"This is the Asian Nation Country"},
+    {title:"India", subtitle:"This is the Asian Nation Country"},
+    {title:"Pakisthan", subtitle:"This is the Asian Nation Country"},
+    {title:"China", subtitle:"This is the Asian Nation Country"},
+    {title:"JAPAN", subtitle:"This is the Asian Nation Country"},
+    {title:"SPAIN", subtitle:"This is the Europian Nation Country"},
+    {title:"Portugal", subtitle:"This is the Europian Nation Country"},
+    {title:"France", subtitle:"This is the Europian Nation Country"},
+    {title:"Poland", subtitle:"This is the Europian Nation Country"},
+    {title:"Indonesia", subtitle:"This is the Asian Nation Country"},
+  ];
+
+  ChildView= ({title, subtitle}) => {
+    return(
+      <View style={{backgroundColor:"white", padding:10, margin:5}}>
+        <Text style={{color:"black", fontSize: 18}}>{title}</Text>
+        <Text style={{color:"black", fontSize: 16}}>{subtitle}</Text>
+      </View>
+    )
+  }
+
+
   render() {
     return (
       <ScrollView>
@@ -30,6 +53,7 @@ class App extends Component {
         {/* <Image style={{flexDirection:"row", justifyContent:"center", height:165, width:"100%"}} source={{uri:'https://www.channelionline.com/wp-content/uploads/2019/07/channel-i-logo-1.png'}} /> */}
           <Image style={{flexDirection:"row", justifyContent:"center", height:165, width:"100%"}} source={require('./images/channel-i-logo.png')} />
         </View>
+
         <View style={{flexDirection:"column", alignItems:"center", height:200, width:"100%", backgroundColor:'red'}}>
         <Text>Example of React Native Core View Component:</Text>
         <View style={{flexDirection:"column", alignItems:"center", height:150, width:"80%", backgroundColor:'green'}}>
@@ -42,6 +66,10 @@ class App extends Component {
         </View>
         </View>
         <Button onPress={this.changeInfo} title="Clcik Here" color="teal"></Button>
+
+        <View>
+          <FlatList data={this.ListDataAsJSON} renderItem={({item}) => <this.ChildView title={item.title} subtitle={item.subtitle} /> } />
+        </View>
 
         <View>
         <Text style={{color:"black"}}>The below Content is Comming from Page and Multiple Components:</Text>
